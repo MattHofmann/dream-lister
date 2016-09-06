ec6b440ea58c164b4a2eabbc1fa5a29167c4d61a
matthiashofmann <mhofm@me.com>
2016-09-07 00:29:32 +0900

    add connection to views

---
+++ DreamLister/ItemCell.swift
@@ -0,0 +1,19 @@
+//
+//  ItemCell.swift
+//  DreamLister
+//
+//  Created by Matthias Hofmann on 07.09.16.
+//  Copyright © 2016 MatthiasHofmann. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+
+    @IBOutlet weak var thumb: UIView!
+    @IBOutlet weak var titel: UILabel!
+    @IBOutlet weak var price: UILabel!
+    @IBOutlet weak var details: UILabel!
+    
+
+}
